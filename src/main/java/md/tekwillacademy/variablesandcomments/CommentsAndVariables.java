2817a95c730ff744289006d7f5e0942b7722a1b7
lguzun <lilia.guzun@gmail.com>
2024-04-12 20:05:17 +0300

    added 2 new boolean variables as a practical exercise

---
+++ src/main/java/md/tekwillacademy/variablesandcomments/CommentsAndVariables.java
@@ -0,0 +1,13 @@
+package md.tekwillacademy.variablesandcomments;
+
+public class CommentsAndVariables {
+    public static void main(String[] args) {
+        boolean isHotOutside;
+        isHotOutside = true;
+        System.out.println("The value of our variable is: "+isHotOutside);
+        boolean isFriday = true;
+        System.out.println("The value of our variable is: "+isFriday);
+    }
+
+
+}
