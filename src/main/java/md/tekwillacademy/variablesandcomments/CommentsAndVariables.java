1da108146de1a3daabdf5f58666d91c5c8913a55
lguzun <lilia.guzun@gmail.com>
2024-04-15 15:17:21 +0300

    added the rest  of variables as a practical exercise

--- src/main/java/md/tekwillacademy/variablesandcomments/CommentsAndVariables.java
+++ src/main/java/md/tekwillacademy/variablesandcomments/CommentsAndVariables.java
@@ -4,9 +4,26 @@ public class CommentsAndVariables {
     public static void main(String[] args) {
         boolean isHotOutside;
         isHotOutside = true;
-        System.out.println("The value of our variable is: "+isHotOutside);
+        System.out.println("The value of isHotOutside variable is: "+isHotOutside);
         boolean isFriday = true;
-        System.out.println("The value of our variable is: "+isFriday);
+        System.out.println("The value of isFriday variable is: "+isFriday);
+        char myFirstCharVariable = 'A';
+        char mySecondCharVariable = 'Z';
+        byte myFirstByteVariable = -128;
+        byte mySecondByteVariable = 127;
+        short myFirstShortVariable = -32768;
+        short mySecondShortVariable = 32767;
+        int myFirstIntVariable = -2147483648;
+        int mySecondIntVariable = 2147483647;
+        long myFirstLongVariable = -9223372036854775808L;
+        long mySecondLongVariable = 9223372036854775807L;
+        float myFirstFloatVariable = -3.4E+38f;
+        float mySecondFloatVariable = 3.14E+38f;
+        double myFirstDoubleVariable = -1.7E+308d;
+        double mySecondDoubleVariable = -1.7E+308d;
+        String myStringVariable = "Lilia";
+        Object myObjectVariable = "Car";
+
     }
 
 
