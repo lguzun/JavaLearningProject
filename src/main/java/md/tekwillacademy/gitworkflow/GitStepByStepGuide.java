69eafd405223e5e1d1e6f22c3eaae8ea6fc6c21c
lguzun <lilia.guzun@gmail.com>
2024-04-15 20:32:26 +0300

    Design the Step By Step Guide GitWorflow

---
+++ src/main/java/md/tekwillacademy/gitworkflow/GitStepByStepGuide.java
@@ -0,0 +1,45 @@
+package md.tekwillacademy.gitworkflow;
+
+public class GitStepByStepGuide {
+    public static void main(String[] args) {
+        String step1 = "Execute - git stash command in order to save the changes";
+        String step2 = "Go to the main branch - git checkout";
+        String step3 = "Get the latest changes - git fetch";
+        String step4 = "Download the differences - git pull";
+        String step5 = "Create new branch - git checkout -b Branch_name";
+        String step6 = "Add code changes for required task";
+        String step7 = "Save the changes - git commit";
+        String step8 = "Upload the changes on the cloud - git push";
+        String step9 = "Go to Github repository";
+        String step10= "Create a new PullRequest into origin/master";
+        String step11= "Invite the collaboratores to review the code";
+        String step12= "Merge the code";
+        String step13= "Delete the created secundary branch as it is already merged";
+        String step14= "Go to IntelijIdea and checkout the master branch - git checkout master";
+        String step14= "download the latest version of origin master";
+
+
+        System.out.println(step1);
+        System.out.println(step2);
+        System.out.println(step3);
+        System.out.println(step4);
+        System.out.println(step5);
+        System.out.println(step6);
+        System.out.println(step7);
+        System.out.println(step8);
+        System.out.println(step9);
+        System.out.println(step10);
+        System.out.println(step11);
+        System.out.println(step12);
+        System.out.println(step13);
+        System.out.println(step14);
+        System.out.println(step15);
+
+
+
+
+
+
+
+    }
+}
